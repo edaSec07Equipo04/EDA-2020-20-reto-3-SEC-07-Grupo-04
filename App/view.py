6dc6f15dabac2294dcce9e278aeef66ccb7b8f9f
sebastian <j.penam@uniandes.edu.co>
2020-09-30 19:37:34 -0500

    pdf terminado

--- App/view.py
+++ App/view.py
@@ -37,8 +37,9 @@ operación seleccionada.
 #  Ruta a los archivos
 # ___________________________________________________
 
+#accidentsFile = "US_Accidents_Dec19.csv"
 accidentsFile = "us_accidents_small.csv"
-
+#accidentsFile = "us_accidents_dis_2016 copy.csv"
 
 # ___________________________________________________
 #  Menu principal
